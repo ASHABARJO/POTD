5a398a1da086af1f8e3ea629f302f1ad527875f6
ASHA BARJO <89405319+ASHABARJO@users.noreply.github.com>
2023-10-03 18:07:12 +0530

    Create colName.cpp

---
+++ colName.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string columnName="";
+        while(n>0){
+            int remainder=(n-1)%26;
+            char digit='A'+remainder;
+            columnName=digit+columnName;
+            n=(n-1)/26;
+        }
+        return columnName;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+
+// } Driver Code Ends
