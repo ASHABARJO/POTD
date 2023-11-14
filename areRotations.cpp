0e8ed6711763bd6ede7caa2aabbe633c1699448c
ASHA BARJO <89405319+ASHABARJO@users.noreply.github.com>
2023-11-14 12:28:57 +0530

    Create areRotations.cpp

---
+++ areRotations.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        string t=s2+s2;
+        return ((t.find(s1)!=string::npos)?1:0);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
