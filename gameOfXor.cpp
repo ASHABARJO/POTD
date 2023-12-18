0887caffa1c84587734a1d086333dfebb130a9ed
ASHA BARJO <89405319+ASHABARJO@users.noreply.github.com>
2023-12-18 11:28:19 +0530

    Create gameOfXor.cpp

---
+++ gameOfXor.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        // code here
+    int cnt=A[0];
+    if(N%2==0)
+    return 0;
+    if(N%2!=0){
+    for(int i=1;i<N;i++){
+        if(i%2==0)
+        cnt=cnt^A[i];
+
+    }
+    }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
